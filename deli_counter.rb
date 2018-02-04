11638e56b4312815d2725bded36005cc3caa88bd
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-04 16:25:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     string = "The line is currently empty."
   else
+    string = "The line is currently: "
     katz_deli.each_with_index do |element, index|
       string = string + "#{index + 1}. " + element + " "
     end
